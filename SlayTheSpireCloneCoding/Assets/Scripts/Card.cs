7631c8e438c08fcb8796942399598a4620ad0f9a
kesa <kesa0v0@gmail.com>
2022-03-25 03:28:44 +0900

    카드 모듈 만듬.
    데이터 받아와서 데이터대로 카드 만들어줌

---
+++ SlayTheSpireCloneCoding/Assets/Scripts/Card.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] SpriteRenderer card;
+    [SerializeField] SpriteRenderer character;
+    [SerializeField] TMP_Text nameTMP;
+    [SerializeField] TMP_Text energyTMP;
+    [SerializeField] Sprite cardFront;
+    [SerializeField] Sprite cardBack;
+
+    public CardData cardData;
+    bool isfront;
+
+    public void Setup(CardData cardData, bool isfront)
+    {
+        this.cardData = cardData;
+        this.isfront = isfront;
+
+        if (this.isfront)
+        {
+            character.sprite = this.cardData.sprite;
+            nameTMP.text = this.cardData.name;
+            energyTMP.text = this.cardData.energy.ToString();
+        }
+        else
+        {
+            card.sprite = cardBack;
+            nameTMP.text = "";
+            energyTMP.text = "";
+        }
+    }
+}
