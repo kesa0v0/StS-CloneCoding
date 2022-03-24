360adec79b60ca1fa72d733cb89c0d19a9bc5c07
kesa <kesa0v0@gmail.com>
2022-03-25 02:19:42 +0900

    added Player, SampleEnemy, SampleCard

---
+++ SlayTheSpireCloneCoding/Assets/Scripts/SO/CardSO.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class CardData
+{
+    public string name;
+    public int energy;
+    public Sprite sprite;
+
+    public int[] Effects; //TOGO: int -> effects
+
+    public void ApplyEffeect(){
+        Debug.Log("EFFECT TEST");
+    }
+}
+
+[CreateAssetMenu(fileName ="CardSO", menuName = "Scriptable Object/CardSO")]
+public class CardSO : ScriptableObject
+{
+    public CardData[] cardDatas;
+}
