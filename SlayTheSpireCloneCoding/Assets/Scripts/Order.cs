3149c7a0901ee99282cd33c97c5b54786cfb8754
kesa <kesa0v0@gmail.com>
2022-03-25 02:36:40 +0900

    주석 추가

--- SlayTheSpireCloneCoding/Assets/Scripts/Order.cs
+++ SlayTheSpireCloneCoding/Assets/Scripts/Order.cs
@@ -9,12 +9,14 @@ public class Order : MonoBehaviour
     [SerializeField] string sortingLayerName;
     int originOrder;
 
+    // 원래 오더 저장
     public void SetOriginOrder(int originOrder)
     {
         this.originOrder = originOrder;
         SetOrder(originOrder);
     }
 
+    // 가장 앞에 오도록 만듬
     public void SetMostFrontOrder(bool isMostFront)
     {
         SetOrder(isMostFront ? 100 : originOrder);
@@ -24,12 +26,12 @@ public class Order : MonoBehaviour
     {
         int mulOrder = order * 10;
 
-        foreach (var renderer in backRenderers)
+        foreach (var renderer in backRenderers) // 카드 본체 오더
         {
             renderer.sortingLayerName = sortingLayerName;
             renderer.sortingOrder = mulOrder;
         }
-        foreach (var renderer in middleRenderers)
+        foreach (var renderer in middleRenderers) // 카드 제목 설명 등 오더
         {
             renderer.sortingLayerName = sortingLayerName;
             renderer.sortingOrder = mulOrder + 1;
