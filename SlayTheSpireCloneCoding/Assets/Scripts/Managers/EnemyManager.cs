da3d6c6c72b6b113c58ecb6f2dd7627701ff1c37
kesa <kesa0v0@gmail.com>
2022-04-11 04:46:52 +0900

    적 정렬 식 약간 조절했는데 더 조절할 필요가 있을듯

--- SlayTheSpireCloneCoding/Assets/Scripts/Managers/EnemyManager.cs
+++ SlayTheSpireCloneCoding/Assets/Scripts/Managers/EnemyManager.cs
@@ -14,24 +14,24 @@ public class EnemyManager : MonoBehaviour
     [SerializeField] Transform enemyLocationRight;
     public List<CharacterEntity> enemyList;
 
-	void EnemyAlignment() // 적 정렬
-	{
-		float targetY = enemyLocation.position.y;
+    void EnemyAlignment() // 적 정렬
+    {
+        float targetY = enemyLocation.position.y;
         float targetXRange = Mathf.Abs(enemyLocationLeft.position.x - enemyLocationRight.position.x);
 
-		for (int i = 0; i < enemyList.Count; i++)
-		{
-			float targetX = enemyLocation.position.x + (enemyList.Count - 1) * -3.4f + i * 6.8f;
+        for (int i = 0; i < enemyList.Count; i++)
+        {
+            float targetX = enemyLocationLeft.position.x + ((i + 1) * targetXRange / (enemyList.Count + 1));
 
-			var targetEntity = enemyList[i];
-			targetEntity.originPos = new Vector3(targetX, targetY, 0);
-			targetEntity.MoveTransform(targetEntity.originPos, true, 0.5f); // TODO: 위치 조정은 확실히 필요할듯 아니면 차라리 적의 크기를 조절하던가
-			targetEntity.GetComponent<Order>()?.SetOriginOrder(i);
-		}
-	}
+            var targetEntity = enemyList[i];
+            targetEntity.originPos = new Vector3(targetX, targetY, 0);
+            targetEntity.MoveTransform(targetEntity.originPos, true, 0.5f); // TODO: 위치 조정은 확실히 필요할듯 아니면 차라리 적의 크기를 조절하던가
+            targetEntity.GetComponent<Order>()?.SetOriginOrder(i);
+        }
+    }
 
 
-    public void SpawnEnemy(EnemyData enemyData, int insertOrder=-1) // 적 스폰 
+    public void SpawnEnemy(EnemyData enemyData, int insertOrder = -1) // 적 스폰 
     { // insertOrder 오른쪽부터 0123
         var enemyObject = Instantiate(enemyPrefab, enemySpawnLocation.position, Utils.QI);
         var enemyComponent = enemyObject.GetComponent<CharacterEntity>();
