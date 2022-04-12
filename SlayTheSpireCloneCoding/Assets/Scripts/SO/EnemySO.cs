90e9f126d61f9d8a4b09ca95506367e58ac36d4b
kesa <kesa0v0@gmail.com>
2022-04-13 01:27:31 +0900

    적 패턴 만들 준비

--- SlayTheSpireCloneCoding/Assets/Scripts/SO/EnemySO.cs
+++ SlayTheSpireCloneCoding/Assets/Scripts/SO/EnemySO.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 [System.Serializable]
 public class EnemyData : CharacterEntityData
 {
-    public int[] pattern; // TODO: 일단 임시로 넣어둔 패턴. 이거 이렇게 해도 될려나? 아님 나중에 따로 적들 스크립트 만들어야 할지도
+    public EnemyPattern[] pattern; // TODO: 일단 임시로 넣어둔 패턴. 이거 이렇게 해도 될려나? 아님 나중에 따로 적들 스크립트 만들어야 할지도
 }
 
 [CreateAssetMenu(fileName ="EnemySO", menuName = "Scriptable Object/EnemySO")]
